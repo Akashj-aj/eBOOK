a78b217d71bdda93265b1a00ed1b058e86ff7b39
Akash Jayaram <akash14035@gmail.com>
2025-04-19 14:07:38 +0530

    Revert "Update supabaseClient.js"
    This reverts commit 1c3f5a86d3146f5d66210c5f3eae3d920693366d.

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,3 +1,6 @@
 
-const client = supabase.createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+const SUPABASE_URL = 'https://gdwvaxnctfxxwwhbhmtl.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkd3ZheG5jdGZ4eHd3aGJobXRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4Njg2MzAsImV4cCI6MjA2MDQ0NDYzMH0.svXFtVsW3VM_Y4VzKKsE7Yt5V5PQcd0FLGlDf-aE3t8';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 window.supabaseClient = client;
